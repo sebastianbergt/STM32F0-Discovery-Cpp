21861097add650c9219f9df4e27cf9b0c722ec22
Sebastian Bergt <sebastian@bergt.org>
2019-07-20 21:55:22 +0200

    initial working example

---
+++ .dep/startup_stm32f051x8.o.d
@@ -0,0 +1,1 @@
+build/startup_stm32f051x8.o: SW4STM32/startup_stm32f051x8.s
---
+++ .dep/stm32f0xx_hal_adc.o.d
@@ -0,0 +1,160 @@
+build/stm32f0xx_hal_adc.o: \
+ Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_adc.c \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h \
+ Inc/stm32f0xx_hal_conf.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h \
+ Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f051x8.h \
+ Drivers/CMSIS/Include/core_cm0.h \
+ /usr/lib/gcc/arm-none-eabi/6.3.1/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/_intsup.h /usr/include/newlib/sys/_stdint.h \
+ Drivers/CMSIS/Include/core_cmInstr.h Drivers/CMSIS/Include/cmsis_gcc.h \
+ Drivers/CMSIS/Include/core_cmFunc.h \
+ Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h \
+ /usr/include/newlib/stdio.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/sys/config.h \
+ /usr/include/newlib/machine/ieeefp.h /usr/include/newlib/sys/cdefs.h \
+ /usr/lib/gcc/arm-none-eabi/6.3.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/6.3.1/include/stdarg.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/types.h \
+ /usr/include/newlib/machine/endian.h \
+ /usr/include/newlib/machine/_endian.h /usr/include/newlib/sys/select.h \
+ /usr/include/newlib/sys/_sigset.h /usr/include/newlib/sys/_timeval.h \
+ /usr/include/newlib/sys/timespec.h /usr/include/newlib/sys/_timespec.h \
+ /usr/include/newlib/machine/types.h /usr/include/newlib/sys/stdio.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc_ex.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h
+
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h:
+
+Inc/stm32f0xx_hal_conf.h:
+
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h:
+
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h:
+
+Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f051x8.h:
+
+Drivers/CMSIS/Include/core_cm0.h:
+
+/usr/lib/gcc/arm-none-eabi/6.3.1/include/stdint.h:
+
+/usr/include/newlib/stdint.h:
+
+/usr/include/newlib/machine/_default_types.h:
+
+/usr/include/newlib/sys/features.h:
+
+/usr/include/newlib/_newlib_version.h:
+
+/usr/include/newlib/sys/_intsup.h:
+
+/usr/include/newlib/sys/_stdint.h:
+
+Drivers/CMSIS/Include/core_cmInstr.h:
+
+Drivers/CMSIS/Include/cmsis_gcc.h:
+
+Drivers/CMSIS/Include/core_cmFunc.h:
+
+Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+
+/usr/include/newlib/stdio.h:
+
+/usr/include/newlib/_ansi.h:
+
+/usr/include/newlib/newlib.h:
+
+/usr/include/newlib/sys/config.h:
+
+/usr/include/newlib/machine/ieeefp.h:
+
+/usr/include/newlib/sys/cdefs.h:
+
+/usr/lib/gcc/arm-none-eabi/6.3.1/include/stddef.h:
+
+/usr/lib/gcc/arm-none-eabi/6.3.1/include/stdarg.h:
+
+/usr/include/newlib/sys/reent.h:
+
+/usr/include/newlib/_ansi.h:
+
+/usr/include/newlib/sys/_types.h:
+
+/usr/include/newlib/machine/_types.h:
+
+/usr/include/newlib/sys/lock.h:
+
+/usr/include/newlib/sys/types.h:
+
+/usr/include/newlib/machine/endian.h:
+
+/usr/include/newlib/machine/_endian.h:
+
+/usr/include/newlib/sys/select.h:
+
+/usr/include/newlib/sys/_sigset.h:
+
+/usr/include/newlib/sys/_timeval.h:
+
+/usr/include/newlib/sys/timespec.h:
+
+/usr/include/newlib/sys/_timespec.h:
+
+/usr/include/newlib/machine/types.h:
+
+/usr/include/newlib/sys/stdio.h:
+
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h:
+
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h:
+
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h:
+
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h:
+
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h:
+
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h:
+
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc.h:
+
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc_ex.h:
+
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h:
+
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h:
+
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h:
+
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h:
+
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h:
+
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h:
+
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h:
+
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h:
